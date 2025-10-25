3ed93dfe4cc84e78a15c7eccaebebab5a16e0860
hunt3r <lyzethan@gmail.com>
2025-10-25 14:16:41 +0800

    Started Object ESP + New Bundle System

---
+++ build/init.lua
@@ -0,0 +1,15 @@
+local fs = require("@lune/fs")
+local process = require("@lune/process")
+
+local FilePath = "dist/main.luau"
+local TempFile = "dist/temp.luau"
+local Header = fs.readFile("./build/header.lua")
+
+local function bundle()
+	process.exec("darklua", { "process", "src/init.luau", TempFile, "--config", "./build/darklua.config.json" })
+	local value = fs.readFile(TempFile)
+	fs.removeFile(TempFile)
+	return value
+end
+
+fs.writeFile(FilePath, Header .. "\n\n" .. bundle())
